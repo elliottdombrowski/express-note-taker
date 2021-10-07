2d8bad02e4f95f594955d4c0f7fc43fad30d8104
elliottdombrowski <elliottroyal@me.com>
2021-10-07 13:09:52 -0500

    just trying things

--- server.js
+++ server.js
@@ -14,9 +14,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) => {
+    res.json(`${req.method} request received`);
+
+    console.info(`${req.method} request recieved`);
+});
+
 
 
 // LISTEN TO PORT
 app.listen(PORT, () =>
-    console.log('App listening at http://localhost:${PORT}')
+    console.log(`App listening at http://localhost:${PORT}`)
 );
