11a4709c0115caa819e17626bd526cee97481cb0
elliottdombrowski <elliottroyal@me.com>
2021-10-09 23:07:22 -0500

    Working on UUID

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
+// REQUIRE MODULE TO GENERATE UNIQUE IDS
+const uuid = () => {
+    return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+};
+
 // REQUIRE JSON DB
 const db = require('./db/db.json');
 const { send } = require('process');
@@ -28,17 +35,24 @@ app.get('/api/notes', (req, res) => {
     res.json(db);
 });
 
+
+// ???? WHEN UUID AT BOTTOM, HTML WON'T UPDATE BUT TEXT IS DEFINED. UUID AT TOP, HTML UPDATES BUT TEXT UNDEFINED
+// ONLY WRITING ID TO DB.JSON
 app.post('/api/notes', (req, res) => {
     let newNote = {
+        note_id: uuid(),
         title: req.query.noteTitle,
         text: req.query.noteText
     }
     db.push(newNote);
 
-    // Save to file?
     console.info(`${req.method} request recieved`);
     
     fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(db), {flag: 'w+'}, () => {
+        res.json(newNote);
+    });
+
+    fs.writeFile(path.join('/assets/notes.html'), JSON.stringify(db), {flag: 'w+'}, () => {
         res.json(newNote);
     });
 });
